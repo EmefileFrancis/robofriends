5979c1215e2fb883226d848410d29da0ba004574
Emefile Francis <emefilefranklin@gmail.com>
2020-01-12 22:19:21 +0100

    Updates

---
+++ precache-manifest.42ee87bdca01482d9bc86924cb6be113.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5ecfb018623fc713a8525db918632e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "902bfc4130dd1c857aba",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "8a539cd44824499922ba",
+    "url": "/robofriends/static/css/main.e83845a9.chunk.css"
+  },
+  {
+    "revision": "902bfc4130dd1c857aba",
+    "url": "/robofriends/static/js/2.a98415d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.a98415d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a539cd44824499922ba",
+    "url": "/robofriends/static/js/main.8eb5c761.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  },
+  {
+    "revision": "b6e9bb37f4e5cd1059e6ebb846a6d40e",
+    "url": "/robofriends/static/media/SegaLogoFont.b6e9bb37.woff"
+  }
+]);
